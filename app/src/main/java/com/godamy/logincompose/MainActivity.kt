6c24a246f1e47cfbd3eb7f45cb74b132362e96f2
Miguel Godoy <g.miguel27@gmail.com>
2022-06-20 23:11:52 +0200

    Animated Content

--- app/src/main/java/com/godamy/logincompose/MainActivity.kt
+++ app/src/main/java/com/godamy/logincompose/MainActivity.kt
@@ -3,12 +3,15 @@ package com.godamy.logincompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.godamy.logincompose.ui.screen.LoginForm
 import com.godamy.logincompose.ui.screen.Main
 
+
+@ExperimentalAnimationApi
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/godamy/logincompose/ui/screen/MainScreen.kt
+++ app/src/main/java/com/godamy/logincompose/ui/screen/MainScreen.kt
@@ -1,19 +1,56 @@
 package com.godamy.logincompose.ui.screen
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.animation.*
+import androidx.compose.animation.AnimatedContentScope.SlideDirection.Companion.Up
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.dp
 
+@ExperimentalAnimationApi
 @Composable
 fun Main() {
     Screen {
-        Box(contentAlignment = Alignment.Center) {
+        var count by remember { mutableStateOf(0) }
+
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterVertically)
+        ) {
             Text(
                 text = "Main Screen",
                 style = MaterialTheme.typography.h5
             )
+            
+            AnimatedContent(
+                targetState = count,
+                transitionSpec = {
+                    slideInVertically { fullHeight -> fullHeight } + fadeIn() with
+                            slideOutVertically { fullHeight -> fullHeight } + fadeOut()
+                }
+            ) {
+                Text(text = "Num of clicks: $count")
+            }
+
+            AnimatedContent(
+                targetState = count,
+                transitionSpec = {
+                    slideIntoContainer(Up) + fadeIn() with
+                            slideOutOfContainer(Up) + fadeOut()
+                }
+            ) {
+                Text(text = "Num of clicks: $count")
+            }
+            
+            Button(onClick = { count++ }) {
+                Text(text = "Count")
+            }
+            
         }
+
     }
 }
