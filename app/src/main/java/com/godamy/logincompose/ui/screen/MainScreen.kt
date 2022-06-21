8512c8e27b690ed7223d23613bc458e703b95628
Miguel Godoy <g.miguel27@gmail.com>
2022-06-21 16:33:29 +0200

    Animated with rememberInfiniteTransition

--- app/src/main/java/com/godamy/logincompose/ui/screen/MainScreen.kt
+++ app/src/main/java/com/godamy/logincompose/ui/screen/MainScreen.kt
@@ -2,13 +2,19 @@ package com.godamy.logincompose.ui.screen
 
 import androidx.compose.animation.*
 import androidx.compose.animation.AnimatedContentScope.SlideDirection.Companion.Up
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.keyframes
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 @ExperimentalAnimationApi
@@ -17,40 +23,57 @@ fun Main() {
     Screen {
         var count by remember { mutableStateOf(0) }
 
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterVertically)
-        ) {
-            Text(
-                text = "Main Screen",
-                style = MaterialTheme.typography.h5
+        val infiniteTransition = rememberInfiniteTransition()
+        val bgColor by infiniteTransition.animateColor(
+            initialValue = Color.White,
+            targetValue = Color.LightGray,
+            animationSpec = infiniteRepeatable(
+                animation = keyframes {
+                    durationMillis = 1000
+                },
+                repeatMode = RepeatMode.Reverse
             )
-            
-            AnimatedContent(
-                targetState = count,
-                transitionSpec = {
-                    slideInVertically { fullHeight -> fullHeight } + fadeIn() with
-                            slideOutVertically { fullHeight -> fullHeight } + fadeOut()
-                }
-            ) {
-                Text(text = "Num of clicks: $count")
-            }
+        )
 
-            AnimatedContent(
-                targetState = count,
-                transitionSpec = {
-                    slideIntoContainer(Up) + fadeIn() with
-                            slideOutOfContainer(Up) + fadeOut()
-                }
+        Box(contentAlignment = Alignment.Center) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterVertically),
+                modifier = Modifier
+                    .wrapContentSize()
+                    .background(bgColor)
+                    .padding(16.dp)
             ) {
-                Text(text = "Num of clicks: $count")
+                Text(
+                    text = "Main Screen",
+                    style = MaterialTheme.typography.h5
+                )
+
+                AnimatedContent(
+                    targetState = count,
+                    transitionSpec = {
+                        slideInVertically { fullHeight -> fullHeight } + fadeIn() with
+                                slideOutVertically { fullHeight -> fullHeight } + fadeOut()
+                    }
+                ) {
+                    Text(text = "Num of clicks: $count")
+                }
+
+                AnimatedContent(
+                    targetState = count,
+                    transitionSpec = {
+                        slideIntoContainer(Up) + fadeIn() with
+                                slideOutOfContainer(Up) + fadeOut()
+                    }
+                ) {
+                    Text(text = "Num of clicks: $count")
+                }
+
+                Button(onClick = { count++ }) {
+                    Text(text = "Count")
+                }
+
             }
-            
-            Button(onClick = { count++ }) {
-                Text(text = "Count")
-            }
-            
         }
-
     }
 }
